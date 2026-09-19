0ee0f998a4b8ee45cb7f7733ce292e3cd1377e0d
Nikhil484144 <f20240992@pilani.bits-pilani.ac.in>
2026-09-19 15:43:01 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,8 +4,18 @@
 module tb;
 
   // TODO: declare the inputs and outputs
+  reg  [2:0] t_sel;   // 3 bits to cover DEPTH=8
+  wire [7:0] t_dout;  // 8 bits to cover WIDTH=8
+  integer i;          // Loop variable
 
   // TODO: instantiate DUT here
+  lut #(
+    .WIDTH(8),
+    .DEPTH(8)
+  ) DUT (
+    .sel(t_sel),
+    .dout(t_dout)
+  );
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -18,10 +28,17 @@ module tb;
 
   initial begin
     // TODO: apply different input combinations
-
+      // Loop through all 8 addresses
+    for (i = 0; i < 8; i = i + 1) begin
+      t_sel = i;
+      #5; // Wait 5 time units to observe the output
+    end
+    $finish;
   end
 
-  initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+ // Print outputs to console
+  initial begin
+    $monitor($time, " | sel=%d | dout=%d", t_sel, t_dout);
+  end
 
 endmodule
